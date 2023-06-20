89da416daf98d963ff9bf62a6597f9621494ee22
Sagar Das <dasagar01@gmai.com>
2023-06-21 00:56:37 +0530

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ChatBubbleIcon from "@mui/icons-material/ChatBubble";
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <ChatBubbleIcon />
+        Keeper
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
